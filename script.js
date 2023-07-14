4be4a0518fc5c0e900021084801a9fd01216a3b4
Krushika <137705754+krushika-25@users.noreply.github.com>
2023-07-14 15:19:56 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+
+const stars = document.querySelectorAll('.stars i');
+const card = document.querySelector('.card');
+
+stars.forEach(function(star) {
+    star.addEventListener('mouseover', function() {
+        card.className = `card ${this.id}`
+    })
+});
+
